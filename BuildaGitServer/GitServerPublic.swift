4333dc297a8ba6870bb05b98602ff5fe84044f13
Rachel Caileff <rcaileff@enova.com>
2016-05-05 14:52:16 -0500

    Implement support for Enterprise GitHub using Personal Tokens. Add .EnterpriseGitHub to GitService enum. Change GitService to use an associated value for .EnterpriseGitHub's host. Get the enterprise server's host from the git repo in the keychain. Add and use GitService.type() because the new version of GitService breaks things like "==" and using the rawValue as a dictionary key. Assert that OAuth authorize URL, access token, client, and secret will not be use for Enterprise GitHub.  Fail horribly if someone tries. Replaced hardcoded strings in WorkspaceMetadata with calls to GitService.foo.hostname(). Move some ProjectViewController.ConfigEditViewController logic into the switch statement because the code had assumed anything other than .BitBucket was .GitHub. Distinguish between an unsupported server and an Enterprise GitHub server in ProjectAuthenticator.fromString.
    Add EnterpriseGitHubTests that are not included in any test targets because they fail without a repo to contact.
    Add tests for ProjectAuthenticator.fromString.

--- BuildaGitServer/GitServerPublic.swift
+++ BuildaGitServer/GitServerPublic.swift
@@ -12,14 +12,24 @@ import Keys
 import ReactiveCocoa
 import Result
 
-public enum GitService: String {
-    case GitHub = "github"
-    case BitBucket = "bitbucket"
+public enum GitService {
+    case GitHub
+    case EnterpriseGitHub(host: String)
+    case BitBucket
 //    case GitLab = "gitlab"
-    
+
+    public func type() -> String {
+        switch self {
+        case .GitHub: return "github"
+        case .EnterpriseGitHub: return "enterprisegithub"
+        case .BitBucket: return "bitbucket"
+        }
+    }
+
     public func prettyName() -> String {
         switch self {
         case .GitHub: return "GitHub"
+        case .EnterpriseGitHub: return "EnterpriseGitHub"
         case .BitBucket: return "BitBucket"
         }
     }
@@ -27,6 +37,7 @@ public enum GitService: String {
     public func logoName() -> String {
         switch self {
         case .GitHub: return "github"
+        case .EnterpriseGitHub: return "enterprisegithub"
         case .BitBucket: return "bitbucket"
         }
     }
@@ -41,6 +52,7 @@ public enum GitService: String {
     public func authorizeUrl() -> String {
         switch self {
         case .GitHub: return "https://github.com/login/oauth/authorize"
+        case .EnterpriseGitHub: assert(false)
         case .BitBucket: return "https://bitbucket.org/site/oauth2/authorize"
         }
     }
@@ -48,6 +60,7 @@ public enum GitService: String {
     public func accessTokenUrl() -> String {
         switch self {
         case .GitHub: return "https://github.com/login/oauth/access_token"
+        case .EnterpriseGitHub: assert(false)
         case .BitBucket: return "https://bitbucket.org/site/oauth2/access_token"
         }
     }
@@ -55,6 +68,7 @@ public enum GitService: String {
     public func serviceKey() -> String {
         switch self {
         case .GitHub: return BuildasaurKeys().gitHubAPIClientId()
+        case .EnterpriseGitHub: assert(false)
         case .BitBucket: return BuildasaurKeys().bitBucketAPIClientId()
         }
     }
@@ -62,6 +76,7 @@ public enum GitService: String {
     public func serviceSecret() -> String {
         switch self {
         case .GitHub: return BuildasaurKeys().gitHubAPIClientSecret()
+        case .EnterpriseGitHub: assert(false)
         case .BitBucket: return BuildasaurKeys().bitBucketAPIClientSecret()
         }
     }
